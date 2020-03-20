c14de5276d6f562b0cad7ed2d7aa4523a45f8d16
Emilie <emilie.lepine@epitech.eu>
2020-03-20 23:45:56 +0100

    created index success page

---
+++ apps/frontend/templates/indexSuccess.php
@@ -0,0 +1,35 @@
+<!-- apps/frontend/modules/job/templates/indexSuccess.php -->
+<?php use_stylesheet('jobs.css') ?>
+
+<h1>Job List</h1>
+
+<table>
+    <thead>
+    <tr>
+        <th>Id</th>
+        <th>Category</th>
+        <th>Type</th>
+        <!-- more columns here -->
+        <th>Created at</th>
+        <th>Updated at</th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php foreach ($jobeet_jobs as $jobeet_job): ?>
+        <tr>
+            <td>
+                <a href="<?php echo url_for('job/show?id='.$jobeet_job->getId()) ?>">
+                    <?php echo $jobeet_job->getId() ?>
+                </a>
+            </td>
+            <td><?php echo $jobeet_job->getCategoryId() ?></td>
+            <td><?php echo $jobeet_job->getType() ?></td>
+            <!-- more columns here -->
+            <td><?php echo $jobeet_job->getCreatedAt() ?></td>
+            <td><?php echo $jobeet_job->getUpdatedAt() ?></td>
+        </tr>
+    <?php endforeach ?>
+    </tbody>
+</table>
+
+<a href="<?php echo url_for('job/new') ?>">New</a>
