16a386da17a27aacaa3605e37bdf5bca7883246a
Emilie <emilie.lepine@epitech.eu>
2020-03-23 09:36:04 +0100

    added view yml

--- apps/frontend/modules/job/templates/showSuccess.php
+++ apps/frontend/modules/job/templates/showSuccess.php
@@ -1,114 +1,39 @@
-<table>
-  <tbody>
-    <tr>
-      <th>Id:</th>
-      <td><?php echo $jobeet_job->get
-Warning: preg_replace(): The /e modifier is no longer supported, use preg_replace_callback instead in E:\projets epitech\tuto-symfo-1-4\lib\vendor\symfony\lib\util\sfToolkit.class.php on line 362
-() ?></td>
-    </tr>
-    <tr>
-      <th>Category:</th>
-      <td><?php echo $jobeet_job->get
-Warning: preg_replace(): The /e modifier is no longer supported, use preg_replace_callback instead in E:\projets epitech\tuto-symfo-1-4\lib\vendor\symfony\lib\util\sfToolkit.class.php on line 362
-() ?></td>
-    </tr>
-    <tr>
-      <th>Type:</th>
-      <td><?php echo $jobeet_job->get
-Warning: preg_replace(): The /e modifier is no longer supported, use preg_replace_callback instead in E:\projets epitech\tuto-symfo-1-4\lib\vendor\symfony\lib\util\sfToolkit.class.php on line 362
-() ?></td>
-    </tr>
-    <tr>
-      <th>Company:</th>
-      <td><?php echo $jobeet_job->get
-Warning: preg_replace(): The /e modifier is no longer supported, use preg_replace_callback instead in E:\projets epitech\tuto-symfo-1-4\lib\vendor\symfony\lib\util\sfToolkit.class.php on line 362
-() ?></td>
-    </tr>
-    <tr>
-      <th>Logo:</th>
-      <td><?php echo $jobeet_job->get
-Warning: preg_replace(): The /e modifier is no longer supported, use preg_replace_callback instead in E:\projets epitech\tuto-symfo-1-4\lib\vendor\symfony\lib\util\sfToolkit.class.php on line 362
-() ?></td>
-    </tr>
-    <tr>
-      <th>Url:</th>
-      <td><?php echo $jobeet_job->get
-Warning: preg_replace(): The /e modifier is no longer supported, use preg_replace_callback instead in E:\projets epitech\tuto-symfo-1-4\lib\vendor\symfony\lib\util\sfToolkit.class.php on line 362
-() ?></td>
-    </tr>
-    <tr>
-      <th>Position:</th>
-      <td><?php echo $jobeet_job->get
-Warning: preg_replace(): The /e modifier is no longer supported, use preg_replace_callback instead in E:\projets epitech\tuto-symfo-1-4\lib\vendor\symfony\lib\util\sfToolkit.class.php on line 362
-() ?></td>
-    </tr>
-    <tr>
-      <th>Location:</th>
-      <td><?php echo $jobeet_job->get
-Warning: preg_replace(): The /e modifier is no longer supported, use preg_replace_callback instead in E:\projets epitech\tuto-symfo-1-4\lib\vendor\symfony\lib\util\sfToolkit.class.php on line 362
-() ?></td>
-    </tr>
-    <tr>
-      <th>Description:</th>
-      <td><?php echo $jobeet_job->get
-Warning: preg_replace(): The /e modifier is no longer supported, use preg_replace_callback instead in E:\projets epitech\tuto-symfo-1-4\lib\vendor\symfony\lib\util\sfToolkit.class.php on line 362
-() ?></td>
-    </tr>
-    <tr>
-      <th>How to apply:</th>
-      <td><?php echo $jobeet_job->get
-Warning: preg_replace(): The /e modifier is no longer supported, use preg_replace_callback instead in E:\projets epitech\tuto-symfo-1-4\lib\vendor\symfony\lib\util\sfToolkit.class.php on line 362
-() ?></td>
-    </tr>
-    <tr>
-      <th>Token:</th>
-      <td><?php echo $jobeet_job->get
-Warning: preg_replace(): The /e modifier is no longer supported, use preg_replace_callback instead in E:\projets epitech\tuto-symfo-1-4\lib\vendor\symfony\lib\util\sfToolkit.class.php on line 362
-() ?></td>
-    </tr>
-    <tr>
-      <th>Is public:</th>
-      <td><?php echo $jobeet_job->get
-Warning: preg_replace(): The /e modifier is no longer supported, use preg_replace_callback instead in E:\projets epitech\tuto-symfo-1-4\lib\vendor\symfony\lib\util\sfToolkit.class.php on line 362
-() ?></td>
-    </tr>
-    <tr>
-      <th>Is activated:</th>
-      <td><?php echo $jobeet_job->get
-Warning: preg_replace(): The /e modifier is no longer supported, use preg_replace_callback instead in E:\projets epitech\tuto-symfo-1-4\lib\vendor\symfony\lib\util\sfToolkit.class.php on line 362
-() ?></td>
-    </tr>
-    <tr>
-      <th>Email:</th>
-      <td><?php echo $jobeet_job->get
-Warning: preg_replace(): The /e modifier is no longer supported, use preg_replace_callback instead in E:\projets epitech\tuto-symfo-1-4\lib\vendor\symfony\lib\util\sfToolkit.class.php on line 362
-() ?></td>
-    </tr>
-    <tr>
-      <th>Expires at:</th>
-      <td><?php echo $jobeet_job->get
-Warning: preg_replace(): The /e modifier is no longer supported, use preg_replace_callback instead in E:\projets epitech\tuto-symfo-1-4\lib\vendor\symfony\lib\util\sfToolkit.class.php on line 362
-() ?></td>
-    </tr>
-    <tr>
-      <th>Created at:</th>
-      <td><?php echo $jobeet_job->get
-Warning: preg_replace(): The /e modifier is no longer supported, use preg_replace_callback instead in E:\projets epitech\tuto-symfo-1-4\lib\vendor\symfony\lib\util\sfToolkit.class.php on line 362
-() ?></td>
-    </tr>
-    <tr>
-      <th>Updated at:</th>
-      <td><?php echo $jobeet_job->get
-Warning: preg_replace(): The /e modifier is no longer supported, use preg_replace_callback instead in E:\projets epitech\tuto-symfo-1-4\lib\vendor\symfony\lib\util\sfToolkit.class.php on line 362
-() ?></td>
-    </tr>
-  </tbody>
-</table>
+<!-- apps/frontend/modules/job/templates/showSuccess.php -->
+<?php use_stylesheet('job.css') ?>
+<?php use_helper('Text') ?>
 
-<hr />
+<div id="job">
+    <h1><?php echo $job->getCompany() ?></h1>
+    <h2><?php echo $job->getLocation() ?></h2>
+    <h3>
+        <?php echo $job->getPosition() ?>
+        <small> - <?php echo $job->getType() ?></small>
+    </h3>
 
-<a href="<?php echo url_for('job/edit?
-Warning: preg_replace(): The /e modifier is no longer supported, use preg_replace_callback instead in E:\projets epitech\tuto-symfo-1-4\lib\vendor\symfony\lib\util\sfToolkit.class.php on line 362
-id='.$jobeet_job->get()) ?>">Edit</a>
-&nbsp;
-<a href="<?php echo url_for('job/index') ?>">List</a>
+    <?php if ($job->getLogo()): ?>
+        <div class="logo">
+            <a href="<?php echo $job->getUrl() ?>">
+                <img src="/uploads/jobs/<?php echo $job->getLogo() ?>"
+                     alt="<?php echo $job->getCompany() ?> logo" />
+            </a>
+        </div>
+    <?php endif ?>
+
+    <div class="description">
+        <?php echo simple_format_text($job->getDescription()) ?>
+    </div>
+
+    <h4>How to apply?</h4>
+
+    <p class="how_to_apply"><?php echo $job->getHowToApply() ?></p>
+
+    <div class="meta">
+        <small>posted on <?php echo $job->getDateTimeObject('created_at')->format('m/d/Y') ?></small>
+    </div>
+
+    <div style="padding: 20px 0">
+        <a href="<?php echo url_for('job/edit?id='.$job->getId()) ?>">
+            Edit
+        </a>
+    </div>
+</div>
